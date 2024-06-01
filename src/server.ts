5109624ab9caefc5fe530c783ae678159c85dc85
pelumiadebayo <adepelumi1996@gmail.com>
2024-06-01 01:43:33 +0100

    certificate use

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import mongoose from 'mongoose';
 import app from './app';
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
+
+// Read the CA certificate
+const ca = [fs.readFileSync('global-bundle.pem')];
+
+// Connection options
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  tlsCAFile: 'rglobal-bundle.pem', // Ensure this file is in your project directory
+};
 
 mongoose.connect(process.env.MONGO_URI!, {
 })
