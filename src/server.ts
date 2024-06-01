4dc8d377a82d1cc4ee34108cbbcd3f2bbb6af8bd
pelumiadebayo <adepelumi1996@gmail.com>
2024-06-01 19:10:48 +0100

    server change

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,10 @@
 import mongoose from 'mongoose';
 import app from './app';
-const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-// Read the CA certificate
-const ca = [fs.readFileSync('global-bundle.pem')];
 
-// Connection options
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  tls: true,
-  tlsCAFile: 'rglobal-bundle.pem', // Ensure this file is in your project directory
-};
-
-mongoose.connect(process.env.MONGO_URI!, {
-})
+mongoose.connect(process.env.MONGO_URI!, {})
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
